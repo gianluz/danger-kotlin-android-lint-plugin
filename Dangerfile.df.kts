598b2d90112479549f64d7b638f8d834184279f0
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-02-09 22:10:15 +0000

    Updated dangerfile and ivy publish test

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,5 +1,9 @@
 @file:DependsOn("./build/libs/danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT.jar")
+//@file:Repository("https://repo.maven.apache.org")
+@file:DependsOn("org.apache.commons:commons-text:1.6")
+@file:DependsOn("com.gianluz:danger-kotlin-android-lint-plugin:0.0.1-SNAPSHOT")
 
+import org.apache.commons.lang3.StringUtils
 import systems.danger.kotlin.*
 import com.gianluz.dangerkotlin.androidlint.AndroidLint
 
@@ -8,3 +12,5 @@ register plugin AndroidLint
 val danger = Danger(args)
 
 AndroidLint.report("./build/resources/test/lint-results.xml")
+
+StringUtils.capitalize("wee")
