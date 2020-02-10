3ca67f4e168fcb26c11f06ea8840864c8d16646a
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-02-10 18:26:35 +0000

    Test valid danger file with deps

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,16 +1,15 @@
-@file:DependsOn("./build/libs/danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT.jar")
+//@file:DependsOn("./build/libs/danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT.jar")
 //@file:Repository("https://repo.maven.apache.org")
 @file:DependsOn("org.apache.commons:commons-text:1.6")
-@file:DependsOn("com.gianluz:danger-kotlin-android-lint-plugin:0.0.1-SNAPSHOT")
+//@file:DependsOn("com.gianluz:danger-kotlin-android-lint-plugin:0.0.1-SNAPSHOT")
 
 import org.apache.commons.lang3.StringUtils
 import systems.danger.kotlin.*
-import com.gianluz.dangerkotlin.androidlint.AndroidLint
 
-register plugin AndroidLint
+//register plugin AndroidLint
 
 val danger = Danger(args)
 
-AndroidLint.report("./build/resources/test/lint-results.xml")
+//AndroidLint.report("./build/resources/test/lint-results.xml")
 
 StringUtils.capitalize("wee")
