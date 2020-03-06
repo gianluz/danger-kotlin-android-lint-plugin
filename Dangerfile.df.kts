fbf622c6c2be7096c1ae24e0e74ab261767ac016
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-03-06 13:09:05 +0000

    Updated project for new danger release

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,19 +1,8 @@
-@file:DependsOn("org.apache.commons:commons-text:1.6")
-//@file:Repository(url="file:///build/libs")
-//@file:DependsOn("/Users/gianlucazuddas/Repos/danger-kotlin-android-lint-plugin/build/libs/danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT.jar")
-//@file:DependsOn("com/gianluz/danger-kotlin-android-lint-plugin/0.0.1-SNAPSHOT/danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT.jar")
-//@file:DependsOn("com.gianluz:danger-kotlin-android-lint-plugin:0.0.1-SNAPSHOT")
-//@file:DependsOn("danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT.jar")
+@file:DependsOn("./build/libs/danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT.jar")
 
-//@file:DependsOn(value="./build/libs/danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT.jar")
-
-import org.apache.commons.text.WordUtils
-import org.jetbrains.kotlin.script.util.*
+import com.gianluz.dangerkotlin.androidlint.AndroidLint
 import systems.danger.kotlin.*
 
 val danger = Danger(args)
 
-warn("Created files ${danger.git.createdFiles.size}")
-warn(WordUtils.capitalize("test"))
-
-//AndroidLint.report("./build/resources/test/lint-results.xml")
+AndroidLint.report("./build/resources/test/lint-results.xml")
