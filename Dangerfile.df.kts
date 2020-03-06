2f8c57a060bf5a005092a38de3e6bb0c28863b37
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-03-06 13:42:16 +0000

    Updated travis, git ignore, using shadowjar to test the plugin locally

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,7 +1,10 @@
-@file:DependsOn("./build/libs/danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT.jar")
+//Uses a fatjar in local
+@file:DependsOn("./build/libs/danger-kotlin-android-lint-plugin-0.0.1-SNAPSHOT-all.jar")
 
 import com.gianluz.dangerkotlin.androidlint.AndroidLint
 import systems.danger.kotlin.*
+
+register plugin AndroidLint
 
 val danger = Danger(args)
 
