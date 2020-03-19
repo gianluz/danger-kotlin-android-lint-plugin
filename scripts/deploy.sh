fcba2350e2422ffe2e3ef8e7b8ea53ff2849e3a4
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-03-19 16:36:08 +0000

    Deploy test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 FILE_ENCRYPTED=secret.asc.enc
 FILE_DECRYPTED=secret.asc
 gpg --version
+gpg2 --version
 echo "Decrypting gpg key..."
 openssl aes-256-cbc -K $encrypted_d8011ea95b1b_key -iv $encrypted_d8011ea95b1b_iv -in $FILE_ENCRYPTED -out $FILE_DECRYPTED -d > /dev/null
 if test -f "$FILE_DECRYPTED"; then
