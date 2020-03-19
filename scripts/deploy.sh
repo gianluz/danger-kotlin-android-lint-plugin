882192d1c53fe2cfc3d3d9a62b1f9e6026c678ff
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-03-19 15:45:23 +0000

    Deploy test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_d8011ea95b1b_key -iv $encrypted_d8011ea95b1b_iv -in secret.asc.enc -out secret.asc -d
-./gradlew publish
+FILE_ENCRYPTED=secret.asc.enc
+FILE_DECRYPTED=secret.asc
+echo "Decrypting gpg key..."
+openssl aes-256-cbc -K $encrypted_d8011ea95b1b_key -iv $encrypted_d8011ea95b1b_iv -in $FILE_ENCRYPTED -out $FILE_DECRYPTED -d > /dev/null
+if test -f "$FILE_DECRYPTED"; then
+    echo "$FILE_ENCRYPTED was decrypted"
+    echo "Publishing artifacts..."
+    ./gradlew publish
+fi
