c80bd6aa17cf50b26ff19f972b6904e7aed293d3
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-03-22 16:14:16 +0000

    Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-FILE_ENCRYPTED=secret.asc.enc
-FILE_DECRYPTED=secret.asc
-gpg --version
-gpg2 --version
-echo "Decrypting gpg key..."
-openssl aes-256-cbc -K $encrypted_d8011ea95b1b_key -iv $encrypted_d8011ea95b1b_iv -in $FILE_ENCRYPTED -out $FILE_DECRYPTED -d > /dev/null
-if test -f "$FILE_DECRYPTED"; then
-    echo "$FILE_ENCRYPTED was decrypted"
-    echo "Publishing artifacts..."
-    ./gradlew publishMavenPublicationToSonatypeRepository
-fi
+gradle wrapper
+./gradlew signMavenPublication
+ls build/libs
+./gradlew publishMavenPublicationToSonatypeRepository
